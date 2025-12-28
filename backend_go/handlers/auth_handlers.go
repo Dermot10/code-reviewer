a6e5f1668ad67ca14b77262a511504a26febc4fa
Dermot <dermotag@hotmail.com>
2025-12-28 18:14:06 +0000

    added auth handler

---
+++ backend_go/handlers/auth_handlers.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+// auth handlers for sign up, sign in
+
+// org id
+// user id
+// user password and email
+
+type AuthHandler struct {
+	logger *slog.Logger
+}
+
+func NewAuthHandler(logger *slog.Logger) *AuthHandler {
+	return &AuthHandler{
+		logger: logger,
+	}
+}
+
+func (h *AuthHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *AuthHandler) GetUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (h *AuthHandler) UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+}
+func (h *AuthHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+}
