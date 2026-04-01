402861d97e5d9c4c98534db5d53e2f4b739db21e
Dermot <dermotag@hotmail.com>
2026-04-01 23:12:21 +0100

    changes to testcontainer setup2

--- backend_go/services/test_setup.go
+++ backend_go/services/test_setup.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/dermot10/code-reviewer/backend_go/models"
-	"github.com/redis/go-redis/v9" // Redis client for service
+	"github.com/redis/go-redis/v9"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	postgrescontainer "github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -19,7 +20,7 @@ import (
 func setUp(t *testing.T) (*gorm.DB, *redis.Client) {
 	ctx := context.Background()
 
-	// Start PostgreSQL and Redis containers
+	// Start PostgreSQL container
 	pg, err := postgrescontainer.Run(ctx,
 		"postgres:15",
 		postgrescontainer.WithDatabase("testdb"),
@@ -31,6 +32,7 @@ func setUp(t *testing.T) (*gorm.DB, *redis.Client) {
 	)
 	require.NoError(t, err)
 
+	// Start Redis container
 	redisC, err := rediscontainer.Run(ctx,
 		"redis:7",
 		testcontainers.WithWaitStrategy(
@@ -39,26 +41,33 @@ func setUp(t *testing.T) (*gorm.DB, *redis.Client) {
 	)
 	require.NoError(t, err)
 
-	// Cleanup
+	// Cleanup containers after test
 	t.Cleanup(func() {
 		pg.Terminate(ctx)
 		redisC.Terminate(ctx)
 	})
 
-	// Connect GORM to Postgres container
+	// Get mapped host port for Postgres
 	pgPort, err := pg.MappedPort(ctx, "5432")
 	require.NoError(t, err)
 
-	connStr := fmt.Sprintf(
-		"host=localhost port=%d user=test dbname=testdb password=test sslmode=disable",
-		pgPort.Int(),
-	)
+	// Retry connecting to Postgres until ready
+	var db *gorm.DB
+	for i := 0; i < 5; i++ {
+		connStr := fmt.Sprintf(
+			"host=localhost port=%d user=test dbname=testdb password=test sslmode=disable",
+			pgPort.Int(),
+		)
+		db, err = gorm.Open(gormpostgres.Open(connStr), &gorm.Config{})
+		if err == nil {
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+	require.NoError(t, err, "failed to connect to Postgres")
 
-	db, err := gorm.Open(gormpostgres.Open(connStr), &gorm.Config{})
-	require.NoError(t, err)
-
-	// Migrate DB table to testcontainer for test usage
-	db.AutoMigrate(
+	// Auto-migrate tables
+	err = db.AutoMigrate(
 		&models.User{},
 		&models.File{},
 		&models.Review{},
@@ -66,8 +75,16 @@ func setUp(t *testing.T) (*gorm.DB, *redis.Client) {
 		&models.ChatMessage{},
 		&models.Conversation{},
 	)
+	require.NoError(t, err)
 
-	// Connect Redis client to Redis container
+	// Insert default test fixtures (users) to prevent FK violations
+	err = db.Create(&models.User{
+		ID:   1,
+		Name: "Test User",
+	}).Error
+	require.NoError(t, err)
+
+	// Connect Redis client using mapped endpoint
 	addr, err := redisC.Endpoint(ctx, "")
 	require.NoError(t, err)
 
